9399da81ccdeb9d4dcac539d372c0b34ef1b02dc
Muhammad Mazher Khan <161349044+muhammadmazher@users.noreply.github.com>
2026-01-29 11:13:24 +0500

    Change greeting from 'class 3' to 'Mazher'

--- class-3-homework/src/app/page.tsx
+++ class-3-homework/src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 export default function Home() {
   return (
     <div className="pb-2">
-      <h1 className="text-2xl text-center py-16" >Hello From class 3</h1>
+      <h1 className="text-2xl text-center py-16" >Hello From Mazher</h1>
     </div>
   );
 }
